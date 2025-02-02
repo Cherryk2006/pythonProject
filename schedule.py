b6d83c4ee2510bb91753f6ea5f4075851e44e9cd
Cherryk2006 <anastasya.cherednyk@gmail.com>
2025-02-02 10:08:25 -0600

    Tests remake

--- schedule.py
+++ schedule.py
@@ -11,24 +11,25 @@ def booking_time_discovery(schedule_start, schedule_end, trainer_bookings, searc
     current_time = start_time
     while current_time + timedelta(minutes=search_window) <= end_time:
         all_time_slots.append(current_time)
-        current_time += timedelta(minutes=15)
-
-    all_time_slots = [slot for slot in all_time_slots if not (slot >= schedule_start and slot + datetime.timedelta(minutes=search_window) <= schedule_end)]
+        current_time += timedelta(minutes=search_window)
 
     for booking in trainer_bookings :
         booking_start = booking[0]
         booking_end = booking[1]
-        all_time_slots = [slot for slot in all_time_slots if not (slot >= booking_start and slot < booking_end)]
+        all_time_slots = [slot for slot in all_time_slots if not (booking_start <= slot < booking_end or booking_start < slot + timedelta(minutes=search_window) <= booking_end)]
 
-        return all_time_slots
+    return all_time_slots
 
 
 if __name__ == '__main__':
-    trainer_id = 1
-    service_id = 1
-    date = datetime.date(2024, 12, 25)
+    schedule_start = datetime.datetime(2024, 12, 25, 9, 0)
+    schedule_end = datetime.datetime(2024, 12, 25, 14, 0)
+    search_window = 60
+    trainer_bookings = [
+        (datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 11, 0),),
+    ]
 
-    available_slots = booking_time_discovery(trainer_id, service_id, date)
+    available_slots = booking_time_discovery(schedule_start, schedule_end, trainer_bookings, search_window)
     print("Available slots for booking:")
     for slot in available_slots:
-        print(slot.strf("%Y-%m-%d %H:%M"))
+        print(slot)
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class TestSchedule(unittest.TestCase):
         schedule_start = datetime.datetime(2024, 12, 25, 9, 0)
         schedule_end = datetime.datetime(2024, 12, 25, 14, 0)
         trainer_bookings = []
-        search_window = 60
+        search_window = 15
         results = schedule.booking_time_discovery(schedule_start, schedule_end, trainer_bookings, search_window)
         expected = [
             datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 15), datetime.datetime(2024, 12, 25, 9, 30), datetime.datetime(2024, 12, 25, 9, 45),
@@ -24,13 +24,13 @@ class TestSchedule(unittest.TestCase):
     def test_schedule_one_booking(self):
         start_date = datetime.datetime(2024, 12, 25, 9, 0)
         end_date = datetime.datetime(2024, 12, 25, 14, 0)
-        search_window = 60
+        search_window = 15
         trainer_bookings = [
             (datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 11, 0),),
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         expected = [
-            datetime.datetime(2024, 12, 25, 9, 0),
+            datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 15), datetime.datetime(2024, 12, 25, 9, 30), datetime.datetime(2024, 12, 25, 9, 45),
             datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45),
             datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 15), datetime.datetime(2024, 12, 25, 12, 30), datetime.datetime(2024, 12, 25, 12, 45),
             datetime.datetime(2024, 12, 25, 13, 0), datetime.datetime(2024, 12, 25, 13, 15), datetime.datetime(2024, 12, 25, 13, 30), datetime.datetime(2024, 12, 25, 13, 45),
@@ -39,34 +39,36 @@ class TestSchedule(unittest.TestCase):
 
         search_window = 30
         expected = [
-            datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 15), datetime.datetime(2024, 12, 25, 9, 30),
-            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45),
-            datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 15), datetime.datetime(2024, 12, 25, 12, 30), datetime.datetime(2024, 12, 25, 12, 45),
-            datetime.datetime(2024, 12, 25, 13, 0), datetime.datetime(2024, 12, 25, 13, 15), datetime.datetime(2024, 12, 25, 13, 30), datetime.datetime(2024, 12, 25, 13, 45),
+            datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 30),
+            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 30),
+            datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 30),
+            datetime.datetime(2024, 12, 25, 13, 0), datetime.datetime(2024, 12, 25, 13, 30),
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         self.assertEqual(expected, results)
 
+        end_date = datetime.datetime(2024, 12, 25, 13, 0)
         search_window = 60
         trainer_bookings = [
             (datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 10, 0),),
         ]
         expected = [
-            datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 10, 15), datetime.datetime(2024, 12, 25, 10, 30), datetime.datetime(2024, 12, 25, 10, 45),
-            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45),
-            datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 15), datetime.datetime(2024, 12, 25, 12, 30), datetime.datetime(2024, 12, 25, 12, 45),
+            datetime.datetime(2024, 12, 25, 10, 0),
+            datetime.datetime(2024, 12, 25, 11, 0),
+            datetime.datetime(2024, 12, 25, 12, 0),
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         self.assertEqual(expected, results)
 
+        end_date = datetime.datetime(2024, 12, 25, 12, 0)
         search_window = 60
         trainer_bookings = [
             (datetime.datetime(2024, 12, 25, 13, 0), datetime.datetime(2024, 12, 25, 14, 0),),
         ]
         expected = [
-            datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 15), datetime.datetime(2024, 12, 25, 9, 30), datetime.datetime(2024, 12, 25, 9, 45),
-            datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 10, 15), datetime.datetime(2024, 12, 25, 10, 30), datetime.datetime(2024, 12, 25, 10, 45),
-            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45),
+            datetime.datetime(2024, 12, 25, 9, 0),
+            datetime.datetime(2024, 12, 25, 10, 0),
+            datetime.datetime(2024, 12, 25, 11, 0),
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         self.assertEqual(expected, results)
@@ -95,11 +97,12 @@ class TestSchedule(unittest.TestCase):
             (datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 11, 0),),
             (datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 10, 0),),
         ]
+        search_window = 15
 
         expected = [
-            (datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45)),
-            (datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 15), datetime.datetime(2024, 12, 25, 12, 30), datetime.datetime(2024, 12, 25, 12, 45)),
-            (datetime.datetime(2024, 12, 25, 13, 0),),
+            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45),
+            datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 15), datetime.datetime(2024, 12, 25, 12, 30), datetime.datetime(2024, 12, 25, 12, 45),
+            datetime.datetime(2024, 12, 25, 13, 0), datetime.datetime(2024, 12, 25, 13, 15), datetime.datetime(2024, 12, 25, 13, 30), datetime.datetime(2024, 12, 25, 13, 45),
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         self.assertEqual(expected, results)
@@ -111,9 +114,9 @@ class TestSchedule(unittest.TestCase):
         ]
 
         expected = [
-            (datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 15), datetime.datetime(2024, 12, 25, 9, 30), datetime.datetime(2024, 12, 25, 9, 45)),
-            (datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 10, 15), datetime.datetime(2024, 12, 25, 10, 30), datetime.datetime(2024, 12, 25, 10, 45)),
-            (datetime.datetime(2024, 12, 25, 11, 0),),
+            datetime.datetime(2024, 12, 25, 9, 0), datetime.datetime(2024, 12, 25, 9, 15), datetime.datetime(2024, 12, 25, 9, 30), datetime.datetime(2024, 12, 25, 9, 45),
+            datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 10, 15), datetime.datetime(2024, 12, 25, 10, 30), datetime.datetime(2024, 12, 25, 10, 45),
+            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45)
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         self.assertEqual(expected, results)
@@ -126,9 +129,9 @@ class TestSchedule(unittest.TestCase):
         ]
 
         expected = [
-            (datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 10, 15), datetime.datetime(2024, 12, 25, 10, 30), datetime.datetime(2024, 12, 25, 10, 45)),
-            (datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45)),
-            (datetime.datetime(2024, 12, 25, 12, 0),),
+            datetime.datetime(2024, 12, 25, 10, 0), datetime.datetime(2024, 12, 25, 10, 15), datetime.datetime(2024, 12, 25, 10, 30), datetime.datetime(2024, 12, 25, 10, 45),
+            datetime.datetime(2024, 12, 25, 11, 0), datetime.datetime(2024, 12, 25, 11, 15), datetime.datetime(2024, 12, 25, 11, 30), datetime.datetime(2024, 12, 25, 11, 45),
+            datetime.datetime(2024, 12, 25, 12, 0), datetime.datetime(2024, 12, 25, 12, 15), datetime.datetime(2024, 12, 25, 12, 30), datetime.datetime(2024, 12, 25, 12, 45),
         ]
         results = schedule.booking_time_discovery(start_date, end_date, trainer_bookings, search_window)
         self.assertEqual(expected, results)
